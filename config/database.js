c6ac51c0bfc2115cf9a2a77d8f887510fbf3b0b9
yoelsteadyv <yoelsteadyv@gmail.com>
2024-11-30 03:31:17 +0700

    ................

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
 
-// const db = new Sequelize("postgres://neondb_owner:AQHV79KrpRzZ@ep-divine-bar-a1x9z12h-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
+const db = new Sequelize("postgresql://toramdb_owner:K3eLFTObaX9V@ep-shiny-bush-a15vrn05.ap-southeast-1.aws.neon.tech/toramdb?sslmode=require")
 
-const db = new Sequelize('neondb', 'neondb_owner', 'AQHV79KrpRzZ', {
-  host: 'ep-divine-bar-a1x9z12h-pooler.ap-southeast-1.aws.neon.tech',
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: { require: true }
-  }
-})
+// const db = new Sequelize('neondb', 'neondb_owner', 'AQHV79KrpRzZ', {
+//   host: 'ep-divine-bar-a1x9z12h-pooler.ap-southeast-1.aws.neon.tech',
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: { require: true }
+//   }
+// })
 // const db = new Sequelize('toramdb', 'postgres', 'postgres', {
 //   host: 'localhost',
 //   dialect: 'postgres'
