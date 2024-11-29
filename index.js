560fa4bf53f140f0d71d8811f318e3ca0d4d5d25
yoelsteadyv <yoelsteadyv@gmail.com>
2024-11-30 04:04:40 +0700

    .........

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-import express from "express"
-import db from "./config/database.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import dotenv from "dotenv"
-// import Users from "./models/UserModel.js"  //buat import di try catch minta otomatis sequelize bikinin tabel users
-import router from "./routes/index.js"
+import express from "express";
+import db from "./config/database.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-// try {
-//   await db.authenticate()
-//   console.log('database connected...')
-//   // await Users.sync() //berhubungan sama import Users diatas
-// } catch (error) {
-//   console.error(error)
-// }
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'anjay'
-  })
-})
-
+// Middleware CORS
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.options("*", cors());
-app.use(cors({ origin: "*", credentials: true }));
-app.use(cookieParser())
-app.use(express.json())
-app.use(router)
 
+// Middleware lainnya
+app.use(cookieParser());
+app.use(express.json());
+
+// Rute tes
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "anjay" });
+});
+
+// Rute utama
+app.use(router);
+
+// Koneksi Database
 db.authenticate()
   .then(() => {
-    console.log('database connected..');
-    return db.sync()
+    console.log("Database connected...");
+    return db.sync();
   })
   .then(() => {
-    console.log('database sync');
-
+    console.log("Database sync");
   })
-  .catch(err => console.log(`error` + err))
+  .catch((err) => console.log("Error: " + err));
 
-app.listen(3001, () => console.log('server running at port 3001'))
+// Jalankan server
+app.listen(3001, () => console.log("Server running at port 3001"));
